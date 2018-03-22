54c642fe9f577494f74c4d3a634935a82480c0e0
Nadzeya Ivanouskaya <nadzeya.ivanouskaya@itechart-group.com>
2018-03-22 16:03:08 +0300

    'd'

--- server.js
+++ server.js
@@ -31,7 +31,7 @@ function handleDisconnect() {
     });                                     // process asynchronous requests in the meantime.
                                             // If you're also serving http, display a 503 error.
     connection.on('error', function(err) {
-        console.log('db error', err);
+        console.log('db error');
         if(err.code === 'PROTOCOL_CONNECTION_LOST') { // Connection to the MySQL server is usually
             handleDisconnect();                         // lost due to either server restart, or a
         } else {                                      // connnection idle timeout (the wait_timeout
@@ -41,8 +41,6 @@ function handleDisconnect() {
 }
 
 handleDisconnect();
-
-
 
 app.use(bodyParser.urlencoded({ extended: false }));
 
