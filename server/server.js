c30341d04b25cfff1450b9640f90af57ab1567c7
Nadzeya Ivanouskaya <nadzeya.ivanouskaya@itechart-group.com>
2018-03-23 12:28:07 +0300

    Structure

--- server/server.js
+++ server/server.js
@@ -44,7 +44,7 @@ app.use(bodyParser.urlencoded({ extended: false }));
 
 app.use(bodyParser.json());
 
-const staticFiles = express.static(path.join(__dirname,'../../client/build'));
+const staticFiles = express.static(path.join(__dirname, 'build'));
 app.use(staticFiles);
 
 app.get('/getAllFilms', (req, res) => {
@@ -57,8 +57,12 @@ app.get('/getAllFilms', (req, res) => {
 app.use(router);
 
 app.get('*', (req, res) => {
-    res.sendFile(path.resolve( '../client/build', 'index.html'));
+    res.sendFile(path.join( __dirname,'build', 'index.html'));
 });
+
+// app.get('*', (req, res) => {
+//     res.sendFile(path.join( __dirname,'../client/build', 'index.html'));
+// });
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
 
