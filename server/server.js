cf17f9157e6db95e66912b0af546d417f0c955c7
Nadzeya Ivanouskaya <nadzeya.ivanouskaya@itechart-group.com>
2018-03-23 10:36:38 +0300

    'g

--- server/server.js
+++ server/server.js
@@ -54,7 +54,7 @@ app.get('/getAllFilms', (req, res) => {
     });
 });
 
-app.get('*', (req, res) => {
+app.get('/*', (req, res) => {
     res.sendFile(path.resolve(__dirname, '../client/build', 'index.html'));
 });
 
