7c1feca8bbb9ed8fe34562b0f8f20cc2ef05adf0
Nadzeya Ivanouskaya <nadzeya.ivanouskaya@itechart-group.com>
2018-03-26 09:21:19 +0300

    change path

--- client/src/Containers/FilmListContainer/index.js
+++ client/src/Containers/FilmListContainer/index.js
@@ -24,8 +24,8 @@ class FilmListContainer extends Component {
     }
 
     getFilmList = async () => {
-        const response = await fetch('http://localhost:3000/getAllFilms');
-        //  'https://salty-island-73231.herokuapp.com/getAllFilms'
+        const response = await fetch('https://salty-island-73231.herokuapp.com/getAllFilms');
+        //  http://localhost:3000/getAllFilms
         const body = await response.json();
         if (response.status !== 200) throw Error(body.message);
         return body;
