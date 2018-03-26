113315dcfc804ffbbd92751f8a40d63669c4236a
Nadzeya Ivanouskaya <nadzeya.ivanouskaya@itechart-group.com>
2018-03-26 11:42:01 +0300

    images

--- client/src/Components/FilmBox/index.js
+++ client/src/Components/FilmBox/index.js
@@ -9,7 +9,7 @@ const FilmBox = (props) => {
     return  (
 
             <Card className={props.classes.main}>
-                {/*<img src={props.poster} className={props.classes.media}/>*/}
+                <img src={props.poster} className={props.classes.media}/>
                 <CardContent>
                     <Typography variant="headline" component="h2">
                         {props.name}
