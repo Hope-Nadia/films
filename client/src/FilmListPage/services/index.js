993cf737b8bdfa86c7b2d6ac403f206282839d01
Nadzeya Ivanouskaya <nadzeya.ivanouskaya@itechart-group.com>
2018-03-30 13:58:56 +0300

    add more info page with text

--- client/src/FilmListPage/services/index.js
+++ client/src/FilmListPage/services/index.js
@@ -1,5 +1,5 @@
 export const getFilmList = async () => {
-    const response = await fetch('https://salty-island-73231.herokuapp.com/films');
+    const response = await fetch('http://localhost:3000/films');
     //  http://localhost:3000/getAllFilms https://salty-island-73231.herokuapp.com/getAllFilms
     // https://salty-island-73231.herokuapp.com/
     const body = await response.json();
