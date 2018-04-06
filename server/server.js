d51d4cbff7e7d9309ee28aa01310dd167361e2d2
Nadzeya Ivanouskaya <nadzeya.ivanouskaya@itechart-group.com>
2018-04-06 10:16:44 +0300

    'heroku'

--- server/server.js
+++ server/server.js
@@ -13,10 +13,11 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use(router);
 
+app.use('/images/', express.static(path.join(__dirname, './images')));
 const staticFiles = express.static(path.join(__dirname, '../client/build'));
 app.use(staticFiles);
 
-app.use('/images/', express.static(path.join(__dirname, './images')));
+
 
 app.use(routes);
 
